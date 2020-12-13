8e0a0081b501d2c0877eb37c8a4f90f49550e70e
Borja Garcia Gonzalez <borja.garciagonzalez@altran.com>
2020-12-13 01:39:39 +0100

    Little touches

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,3 @@
-/*
---- Day 3: Toboggan Trajectory ---
-With the toboggan login problems resolved, you set off toward the airport. While travel by toboggan might be easy, it's certainly not safe: there's very minimal steering and the area is covered in trees. You'll need to see which angles will take you near the fewest trees.
-
-Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
-
-..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#
-These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
-
-..##.........##.........##.........##.........##.........##.......  --->
-#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
-.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
-..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
-.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
-..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
-.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
-.#........#.#........#.#........#.#........#.#........#.#........#
-#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
-#...##....##...##....##...##....##...##....##...##....##...##....#
-.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
-You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
-
-The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
-
-From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
-
-The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
-
-..##.........##.........##.........##.........##.........##.......  --->
-#..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
-.#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
-..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
-.#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
-..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
-.#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
-.#........#.#........X.#........#.#........#.#........#.#........#
-#.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
-#...##....##...##....##...#X....##...##....##...##....##...##....#
-.#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
-In this example, traversing the map using this slope would cause you to encounter 7 trees.
-
-Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
-
-Your puzzle answer was 178.
-
---- Part Two ---
-Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
-
-Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
-
-Right 1, down 1.
-Right 3, down 1. (This is the slope you already checked.)
-Right 5, down 1.
-Right 7, down 1.
-Right 1, down 2.
-In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
-
-What do you get if you multiply together the number of trees encountered on each of the listed slopes?
-
-Your puzzle answer was 3492520200.
-
-*/
-
 fn main() {
     let data = "..#.#...#.#.#.##.....###.#....#
 ...........##.#...#.#..........
@@ -395,28 +322,34 @@ fn main() {
 #..............#....#.#....###.
 ..##..#..#...#...##........##..
 ..#.##....#..#......###..#.....";
-  let data: Vec<String> = data.split('\n').map(|s| s.to_string()).collect();
+    let data: Vec<String> = data.split('\n').map(|s| s.to_string()).collect();
 
-  println!("The number of trees found in the trajectory was {}", trees_in_trajectory(&data, 3, 1));
+    println!(
+        "The number of trees found in the trajectory was {}",
+        trees_in_trajectory(&data, 3, 1)
+    );
 
-  let mut strategies = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-  let mut counts: Vec<u32> = Vec::new();
-  for strategy in &mut strategies {
-    counts.push(trees_in_trajectory(&data, strategy.0, strategy.1));
-  }
-  println!("The answer to the second question is {}", counts.iter().product::<u32>());
+    let mut strategies = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut counts: Vec<u32> = Vec::new();
+    for strategy in &mut strategies {
+        counts.push(trees_in_trajectory(&data, strategy.0, strategy.1));
+    }
+    println!(
+        "The answer to the second question is {}",
+        counts.iter().product::<u32>()
+    );
 }
 
 fn trees_in_trajectory(map: &Vec<String>, dx: usize, dy: usize) -> u32 {
-  let mut column = 0;
-  let mut row = 0;
-  let mut count = 0;
-  while row < map.len() {
-    if map[row].chars().nth(column%map[row].len()).unwrap() == '#'{
-      count+=1;
+    let mut column = 0;
+    let mut row = 0;
+    let mut count = 0;
+    while row < map.len() {
+        if map[row].chars().nth(column % map[row].len()).unwrap() == '#' {
+            count += 1;
+        }
+        column += dx;
+        row += dy;
     }
-    column += dx;
-    row += dy;
-  }
-  count
+    count
 }
