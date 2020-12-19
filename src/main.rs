41d78f7ff4497e9f139d5093e49b7a14ca9e3c57
Borja Garcia Gonzalez <borja.garciagonzalez@altran.com>
2020-12-19 22:38:10 +0100

    Nineteenth day. First part

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,57 @@
+use regex::Regex;
+use std::collections::HashMap;
+
 fn main() {
-    let data = String::from_utf8_lossy(include_bytes!("data.txt")).replace(" ", "");
-    let data: Vec<&str> = data.split("\n").filter(|x| x.len() > 0).collect();
+    let data = String::from_utf8_lossy(include_bytes!("data.txt")).replace("\"", "");
+    let data: Vec<Vec<&str>> = data
+        .split("\n\n")
+        .map(|x| x.split("\n").filter(|x| x.len() > 0).collect())
+        .collect();
+    assert_eq!(2, data.len());
+    let rules: HashMap<usize, &str> = data[0]
+        .iter()
+        .map(|x| {
+            (
+                x.split(": ").next().unwrap().parse::<usize>().unwrap(),
+                x.split(": ").nth(1).unwrap(),
+            )
+        })
+        .collect();
+    let data = &data[1];
 
-    let mut acc = 0;
-    for eq in &data {
-        let res = solve1(&eq);
-        acc += res;
-        println!("{} solves to {}", eq, res);
-    }
-    println!("Solution {}", acc);
+    println!("{:?}", rules);
+    println!("{:?}", data);
 
-    println!("");
-
-    let mut acc = 0;
-    for eq in &data {
-        let res = solve2(&eq);
-        acc += res;
-        println!("{} solves to {}", eq, res);
-    }
-    println!("Solution {}", acc);
-}
-
-fn find_closing(s: &str) -> Option<usize> {
-    let mut depth = 0;
-    for (idx, c) in s.chars().enumerate() {
-        match c {
-            '(' => depth += 1,
-            ')' => {
-                depth -= 1;
-                if depth == 0 {
-                    return Some(idx);
+    let regexes: HashMap<usize, String> =
+        rules.iter().map(|(k, v)| (*k, solve(&rules, v))).collect();
+    println!("{:?}", regexes);
+    println!(
+        "Data lines matching rule 0: {}",
+        data.iter()
+            .filter(|x| {
+                match Regex::new(&format!("^{}$", regexes.get(&0).unwrap()))
+                    .unwrap()
+                    .captures(x)
+                {
+                    Some(_) => true,
+                    None => false,
                 }
-            }
-            _ => (),
-        }
-    }
-    None
+            },)
+            .count()
+    );
 }
 
-fn find_1st_level_op(s: &str, needles: Vec<char>, rev: bool) -> Option<usize> {
-    let mut depth = 0;
-    let mut iter = s.chars().enumerate().collect::<Vec<(usize, char)>>();
-    if rev {
-        iter = iter
-            .iter()
-            .rev()
-            .map(|&x| x)
-            .collect::<Vec<(usize, char)>>();
-    }
-    for (idx, c) in iter {
-        match c {
-            '(' => depth += 1,
-            ')' => depth -= 1,
-            a if needles.contains(&a) => {
-                if depth == 0 {
-                    return Some(idx);
-                }
+fn solve(rules: &HashMap<usize, &str>, rule: &str) -> String {
+    rule.split(" ")
+        .map(|x| {
+            if x.parse::<usize>().is_ok() {
+                format!(
+                    "({})",
+                    solve(rules, rules.get(&x.parse::<usize>().unwrap()).unwrap())
+                )
+            } else {
+                x.to_string()
             }
-            _ => (),
-        }
-    }
-    None
-}
-
-fn solve1(s: &str) -> i64 {
-    if s.starts_with("(") {
-        let closingidx = find_closing(s).unwrap();
-        if closingidx == s.len() - 1 {
-            return solve1(&s[1..s.len() - 1]);
-        }
-    }
-    let opidx = find_1st_level_op(s, vec!['*', '+'], true);
-    match opidx {
-        Some(idx) => match s.chars().nth(idx).unwrap() {
-            '*' => solve1(&s[0..idx]) * solve1(&s[idx + 1..]),
-            '+' => solve1(&s[0..idx]) + solve1(&s[idx + 1..]),
-            _ => unreachable!(),
-        },
-        None => s.parse::<i64>().unwrap(),
-    }
-}
-
-fn solve2(s: &str) -> i64 {
-    if s.starts_with("(") {
-        let closingidx = find_closing(s).unwrap();
-        if closingidx == s.len() - 1 {
-            return solve2(&s[1..s.len() - 1]);
-        }
-    }
-    let prodidx = find_1st_level_op(s, vec!['*'], false);
-    match prodidx {
-        Some(idx) => solve2(&s[0..idx]) * solve2(&s[idx + 1..]),
-        None => {
-            let sumidx = find_1st_level_op(s, vec!['+'], false);
-            match sumidx {
-                Some(idx) => solve2(&s[0..idx]) + solve2(&s[idx + 1..]),
-                None => s.parse::<i64>().unwrap(),
-            }
-        }
-    }
+        })
+        .collect::<String>()
 }
