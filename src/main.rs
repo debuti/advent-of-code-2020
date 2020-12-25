d0987764046fd720be57746a8bc746dd427d99de
Borja Garcia Gonzalez <borja.garciagonzalez@altran.com>
2020-12-25 02:21:24 +0100

    Twenty-fourth day

--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,121 @@
-use std::convert::TryFrom;
+// use piston_window::*;
+use std::collections::HashSet;
 
-const DEBUG: bool = false;
+const DEBUG: bool = true;
 macro_rules! debugln {
     ($($args:expr),*) => ( if DEBUG {println!($( $args ),* )});
 }
 
-#[derive(Debug)]
-struct CircVec<T> {
-    head: T,
-    data: Vec<T>,
-    capacity: usize,
-    highestv: T,
+fn main() {
+    let data: String = String::from_utf8_lossy(include_bytes!("data.txt"))
+        .replace("se", "m")
+        .replace("sw", "b")
+        .replace("ne", "i")
+        .replace("nw", "y")
+        .to_string();
+    let data: Vec<&str> = data.split("\n").filter(|&x| x.len() > 0).collect();
+
+    part1(&data);
+    part2(&data);
 }
 
-// We need a trait which tells us the "one" value for any type
-trait Increment {
-    fn one() -> Self;
-    fn zero() -> Self;
-}
-
-impl Increment for i32 {
-    fn one() -> Self {
-        1
-    }
-    fn zero() -> Self {
-        0
-    }
-}
-
-impl<
-        T: Copy
-            + PartialEq
-            + std::ops::Add<Output = T>
-            + std::ops::Sub<Output = T>
-            + std::ops::Rem<Output = T>
-            + Increment
-            + std::fmt::Debug,
-    > CircVec<T>
-{
-    fn headpos(&self) -> usize {
-        self.data.iter().position(|&x| x == self.head).unwrap()
-    }
-
-    fn pick3(&mut self) -> [T; 3] {
-        let head = self.headpos() + 1;
-        [
-            if head < self.data.len() {
-                self.data.remove(head)
-            } else {
-                self.data.remove(0)
-            },
-            if head < self.data.len() {
-                self.data.remove(head)
-            } else {
-                self.data.remove(0)
-            },
-            if head < self.data.len() {
-                self.data.remove(head)
-            } else {
-                self.data.remove(0)
-            },
+fn part2(data: &Vec<&str>) {
+    fn adj(coord: &(i32, i32, i32)) -> Vec<(i32, i32, i32)> {
+        vec![
+            (coord.0 - 1, coord.1 + 1, coord.2 + 0),
+            (coord.0 + 0, coord.1 + 1, coord.2 - 1),
+            (coord.0 + 1, coord.1 + 0, coord.2 - 1),
+            (coord.0 + 1, coord.1 - 1, coord.2 + 0),
+            (coord.0 + 0, coord.1 - 1, coord.2 + 1),
+            (coord.0 - 1, coord.1 + 0, coord.2 + 1),
         ]
     }
-
-    fn destcup(&mut self) -> usize {
-        let mut needle = self.head;
-        loop {
-            needle = ((needle - T::one()) + self.highestv) % (self.highestv);
-            if needle == T::zero() {
-                needle = self.highestv;
-            }
-            if self.data.contains(&needle) {
-                debugln!("destination: {:?}", needle);
-                return self.data.iter().position(|&x| x == needle).unwrap();
-            }
-        }
+    fn adjc(flipped: &HashSet<(i32, i32, i32)>, coord: &(i32, i32, i32)) -> usize {
+        adj(coord).iter().filter(|x| flipped.contains(x)).count()
     }
 
-    fn reinsert3_and_move_head(&mut self, new: [T; 3]) {
-        let pos = self.destcup();
-        for item in new.iter().rev() {
-            self.data.insert(pos + 1, *item);
+    let mut flipped: HashSet<(i32, i32, i32)> = HashSet::new();
+    for tile in data {
+        let mut coord = (0, 0, 0);
+        for c in tile.chars() {
+            coord = match c {
+                'w' => (coord.0 - 1, coord.1 + 1, coord.2 + 0),
+                /*nw*/ 'y' => (coord.0 + 0, coord.1 + 1, coord.2 - 1),
+                /*ne*/ 'i' => (coord.0 + 1, coord.1 + 0, coord.2 - 1),
+                'e' => (coord.0 + 1, coord.1 - 1, coord.2 + 0),
+                /*se*/ 'm' => (coord.0 + 0, coord.1 - 1, coord.2 + 1),
+                /*sw*/ 'b' => (coord.0 - 1, coord.1 + 0, coord.2 + 1),
+                _ => unreachable!(),
+            };
         }
-        self.head = self.data[(self.headpos() + 1) % self.data.len()];
+        if flipped.contains(&coord) {
+            flipped.remove(&coord);
+        } else {
+            flipped.insert(coord);
+        }
     }
+    debugln!("Flipped {:?}", flipped.len());
+
+    for day in 1..101 {
+        let lastday = flipped.clone();
+        for black in &lastday {
+            // Check blacks rule
+            {
+                let adjc = adjc(&lastday, &black);
+                if adjc == 0 || adjc > 2 {
+                    flipped.remove(&black);
+                }
+            }
+            // Check whites rule for every adj tile
+            {
+                for adj in adj(&black) {
+                    if !lastday.contains(&adj) && adjc(&lastday, &adj) == 2 {
+                        flipped.insert(adj);
+                    }
+                }
+            }
+        }
+        debugln!("Day {:?}: {}", day, flipped.len());
+    }
+    println!("Step 2 answer: {:?}", flipped.len());
+    //TODO: Use piston to render the board dynamically
+    // let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+    //     .exit_on_esc(true)
+    //     .build()
+    //     .unwrap();
+    // while let Some(e) = window.next() {
+    //     window.draw_2d(&e, |c, g, _device| {
+    //         clear([1.0; 4], g);
+    //         rectangle(
+    //             [1.0, 0.0, 0.0, 1.0], // red
+    //             [0.0, 0.0, 100.0, 100.0],
+    //             c.transform,
+    //             g,
+    //         );
+    //     });
+    // }
 }
 
-impl std::iter::FromIterator<i32> for CircVec<i32> {
-    fn from_iter<I: IntoIterator<Item = i32>>(iter: I) -> Self {
-        let mut c = CircVec {
-            data: Vec::new(),
-            capacity: 0,
-            head: 0,
-            highestv: 0,
-        };
-
-        for i in iter {
-            c.data.push(i);
+fn part1(data: &Vec<&str>) {
+    let mut flipped: HashSet<(i32, i32)> = HashSet::new();
+    for tile in data {
+        let mut coord = (0, 0);
+        for c in tile.chars() {
+            coord = match c {
+                'w' => (coord.0 - 2, coord.1 + 0),
+                'y' => (coord.0 - 1, coord.1 + 3),
+                'i' => (coord.0 + 1, coord.1 + 3),
+                'e' => (coord.0 + 2, coord.1 + 0),
+                'm' => (coord.0 + 1, coord.1 - 3),
+                'b' => (coord.0 - 1, coord.1 - 3),
+                _ => unreachable!(),
+            }
         }
-        c.highestv = *c.data.iter().max().unwrap();
-        c.head = c.data[0];
-        let mut it = c.highestv;
-        while c.data.len() < 1000000 {
-            it += 1;
-            c.data.push(it);
+        if flipped.contains(&coord) {
+            flipped.remove(&coord);
+        } else {
+            flipped.insert(coord);
         }
-        c.highestv = 1000000;
-        c.capacity = c.data.len();
-        c
     }
-}
-
-fn main() {
-    let _test = "389125467";
-    let _data = "653427918";
-    let mut data: CircVec<i32> = _data
-        .chars()
-        .map(|x| i32::try_from(x.to_digit(10).unwrap()).unwrap())
-        .collect();
-
-    for moveid in 0..10000000 {
-        println!("-- move {} --", moveid);
-        debugln!("cups: {:?}", data);
-        let removed = data.pick3();
-        debugln!("pick up: {:?}", removed);
-        data.reinsert3_and_move_head(removed);
-        debugln!("\n");
-    }
-    debugln!("-- final --");
-    println!("cups: {:?}", data);
+    println!("Step 1 answer: {:?}", flipped.len());
 }
